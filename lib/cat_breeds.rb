fc0042db11ea7018a26709ad425817886fceb6a5
Deirdre  Sullivan <dsulliv4@binghamton.edu>
2020-04-18 05:49:52 +0000

    environment update

--- lib/cat_breeds.rb
+++ lib/cat_breeds.rb
@@ -1,4 +1,10 @@
-require "cat_breeds/version"
+require_relative "./pump_selection/version"
+require_relative "./pump_selection/breeds"
+require_relative "./pump_selection/scraper"
+require_relative "./pump_selection/cli"
+require 'open-uri'
+require 'pry'
+require 'nokogiri'
 
 module CatBreeds
   class Error < StandardError; end
