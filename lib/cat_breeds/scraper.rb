44b00e82ab38b2e5b557b72f8945039c4f7c3783
Deirdre  Sullivan <dsulliv4@binghamton.edu>
2020-04-19 05:57:12 +0000

    updated scraper

--- lib/cat_breeds/scraper.rb
+++ lib/cat_breeds/scraper.rb
@@ -1,17 +1,46 @@
 class CatBreeds::Scraper
 
-  def self.scrape 
-    @doc = Nokogiri::HTML(open("https://cattime.com/cat-breeds"))
-    @scraping_block = @doc.css("div.listingByBlockContainer div.categoryBlock")
-  end
+	def self.scrape_index(url) #creates and returns an array of hashes which contain cat breeds and page urls
+		cat_breeds = []
+		doc = Nokogiri::HTML(open(url))
+		breeds = doc.css("#hub-breed-list-container ul li a")
+		breeds.each do |b|
+			breed = {}
+			breed[:name] = b.text
+			breed[:page_url] = b.attr("href")
+			cat_breeds << breed
+		end
+		cat_breeds
+	end
 
-  def self.scrape_product
-        @scraping_block.each do |ele|
-        name = ele.css("a.desc").text.strip
-        series = ele.css("div.sub ul li").text.strip
-        product = PumpSelection::Product.new(name, series)
-        CatBreeds::Product.all << product
-        # binding.pry
-    end
-  end
+	def self.scrape_profile(url) #creates and returns a hash of a cat breed with more information for the user to view
+		breed = {}
+		doc = Nokogiri::HTML(open(url))
+		breed[:blurb] = doc.css("#breed-detail p").text.gsub("\n","").gsub("\t","").gsub("\r","").strip
+		breed[:fun_fact] = doc.css(".interesting-breed-fact p").text.gsub("\n","").gsub("\t","").gsub("\r","").strip
+		breed[:description] = doc.css("#overview .richtext  p").text.gsub("\n","").gsub("\t","").gsub("\r","").gsub(" If the video doesn't start playing momentarily, please install the latest version of Flash.","").strip
+		breed[:history] = doc.css("#history .richtext  p").text.gsub("\n","").gsub("\t","").gsub("\r","").strip
+		breed[:personality] = doc.css("#personality .richtext  p").text.gsub("\n","").gsub("\t","").gsub("\r","").strip
+		breed[:health] = doc.css("#health .richtext  p").text.gsub("\n","").gsub("\t","").gsub("\r","").strip
+		breed[:grooming] = doc.css("#grooming .richtext  p").text.gsub("\n","").gsub("\t","").gsub("\r","").strip
+		breed[:characteristics] = scrape_characteristics(url)
+		breed
+	end
+
+	def self.scrape_characteristics(url) #creates and returns a hash of breed characteristics to be used in self.scrape_profile
+		characteristics = {}
+		doc = Nokogiri::HTML(open(url))
+
+		keys = []
+		doc.css("td.title").each {|key| keys << key.text.scan(/\A(\w+\s?\w+)/)[0][0].gsub(" ","_")}
+		values = []
+		doc.css("td.rating").each{|value| values << value.text}
+		keys.each_with_index do |key,i|
+			characteristics[key.to_sym] = values[i]
+		end
+
+		characteristics.sort_by{|k,v|k}
+	end
+
+
 end
