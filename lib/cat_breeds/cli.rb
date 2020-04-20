b2ddb8b7cd00a5ad0c0da570a5a4a3fbbd016060
Deirdre  Sullivan <dsulliv4@binghamton.edu>
2020-04-20 06:05:18 +0000

    more

--- lib/cat_breeds/cli.rb
+++ lib/cat_breeds/cli.rb
@@ -1,19 +1,7 @@
+
+
 CatBreeds::Cli 
 
 def call 
   puts "Welcome to the Cat Catalogue"
-  get_cats
-  list_cats
-  get_user_cats
 end 
-
-def get_cats
-end 
-
-def list_cats
-end 
-
-def get_user_cats
-end 
-
-
