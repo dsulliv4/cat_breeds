4f93bade9225cc9be043cb27d6c57f5d41e5c82a
Deirdre  Sullivan <dsulliv4@binghamton.edu>
2020-04-21 06:29:37 +0000

    environment setup

---
+++ lib/cat_breeds/environnment.rb
@@ -0,0 +1,9 @@
+require_relative "./cat_breeds/version"
+require_relative "./cat_breeds/breeds"
+require_relative "./cat_breeds/scraper"
+require_relative "./cat_breeds/cli"
+require 'open-uri'
+require 'pry'
+require 'nokogiri'
+
+
