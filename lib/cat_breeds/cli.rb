e588ac74f4bf26e2a73e085c2888276a19583d8f
Deirdre  Sullivan <dsulliv4@binghamton.edu>
2020-04-22 23:27:05 +0000

    updated cli

--- lib/cat_breeds/cli.rb
+++ lib/cat_breeds/cli.rb
@@ -1,7 +1,17 @@
 class CatBreeds::Cli 
 
-def call 
-  puts "Welcome to the Cat Catalogue"
-end 
+  def welcome
+    puts "__________________________________________________________________"
+    puts " "
+    puts "---------------- Welcome to the Cat Catalogue -------------------"
+    puts "__________________________________________________________________"
+      sleep (2)
+    puts "Please choose one of the following options"
+    puts "***************************************"
 
-end 
+    puts " __ Please type 'show list of breed types' for the full list of cat breeds"
+
+  puts " __ Type 'close' to exit the application"
+  
+  
+  end
