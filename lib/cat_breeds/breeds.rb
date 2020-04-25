4ac69d8069c76fd6c8ae0b50814eadd774c42261
dsulliv4 <34930950+dsulliv4@users.noreply.github.com>
2020-04-25 18:52:19 -0400

    update breeds

--- lib/cat_breeds/breeds.rb
+++ lib/cat_breeds/breeds.rb
@@ -1,29 +1,22 @@
-class Breeds
+class CatBreeds::Breeds
   attr_accessor :name, :url 
 
-@@breeds =[]
-
-  def self.scraper
-   Scraper.scrape_breeds
-    @@breeds 
-  end 
-
-def initialize (breed_name, breed_url)
-  @name = breed_name
-  @url = breed_url 
-
-save
-end 
-
-def save 
-  @@breeds << self 
-end 
-
-def self.all 
-  @@breeds
-end 
+	@@breeds =[]
 
 
+	def initialize (breed_name, breed_url)
+	  @name = breed_name
+	  @url = breed_url 
 
-  
+	save
+	end 
+
+	def save 
+	  @@breeds << self 
+	end 
+
+	def self.all 
+	  @@breeds
+	end 
+end
   
