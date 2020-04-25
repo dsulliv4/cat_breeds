e4a278b85616e5d4d5cdb268b85314e57a19173e
dsulliv4 <34930950+dsulliv4@users.noreply.github.com>
2020-04-25 18:50:39 -0400

    Used scrapper in cli

--- lib/cat_breeds/cli.rb
+++ lib/cat_breeds/cli.rb
@@ -1,19 +1,45 @@
-class CatBreeds::Cli 
+module CatBreeds
+  class Cli 
+    def call
+      puts "__________________________________________________________________"
+      puts " "
+      puts "---------------- Welcome to the Cat Catalogue -------------------"
+      puts "__________________________________________________________________"
+        sleep (2)
+      puts "Please choose one of the following options"
+      puts "***************************************"
 
-  def call
-    puts "__________________________________________________________________"
-    puts " "
-    puts "---------------- Welcome to the Cat Catalogue -------------------"
-    puts "__________________________________________________________________"
-      sleep (2)
-    puts "Please choose one of the following options"
-    puts "***************************************"
+      puts " __ Please type 'show list' for the full list of cat breeds"
+      
+      puts " __ Type 'close' to exit the application"
 
-    puts " __ Please type 'show list' for the full list of cat breeds"
+      input = gets.chomp 
 
-  puts " __ Type 'close' to exit the application"
-  
-  
-  end
-  
-end 
+      if input == 'show list'
+        puts "All breeds"
+
+        CatBreeds::Scraper.scrape_breeds
+        CatBreeds::Breeds.all.each.with_index do |breed, index|
+          puts "#{index} - #{breed.name}"
+        end
+
+        print 'Please select a breed: '
+        breed = gets.chomp.to_i 
+        selected_breed = CatBreeds::Breeds.all[breed]
+
+
+        puts "You selected #{selected_breed.name}"
+        puts "Getting breed info"
+
+        breed_info = CatBreeds::Scraper.scrape_breed_info(selected_breed.url)
+        puts breed_info
+
+      elsif input = "close"
+
+        exit
+      end
+    
+    
+    end
+  end 
+end
