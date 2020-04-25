9365eb994010b3891962796d2be4ce1d8d8e0e74
dsulliv4 <34930950+dsulliv4@users.noreply.github.com>
2020-04-25 18:53:16 -0400

    update scraper

--- lib/cat_breeds/scraper.rb
+++ lib/cat_breeds/scraper.rb
@@ -1,17 +1,17 @@
-class Scraper
+class CatBreeds::Scraper
   def self.scrape_breeds
     doc = Nokogiri::HTML(open("https://cattime.com/cat-breeds"))
     doc.css('.article-crumbs .list-item-title').map do |item|
       breed_name = item.text 
       breed_url = item.attributes['href'].value
 
-       Breeds.new(breed_name, breed_url)
+       CatBreeds::Breeds.new(breed_name, breed_url)
     end
   end
 
    def self.scrape_breed_info(url)
-
-    doc = Nokogiri::HTML(open(breed.url))
+    doc = Nokogiri::HTML(open(url))
     doc.css('.breeds-single-intro p:first').text
   end
 end
+
