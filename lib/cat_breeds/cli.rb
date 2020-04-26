34dc0c9350bab4677696652b2864bd23bf39e6bf
Deirdre  Sullivan <dsulliv4@binghamton.edu>
2020-04-26 09:21:06 +0000

    updated cli

--- lib/cat_breeds/cli.rb
+++ lib/cat_breeds/cli.rb
@@ -1,6 +1,12 @@
 module CatBreeds
   class Cli 
     def call
+      welcome 
+      puts "   "
+      user_input
+    end
+
+      def welcome
       puts "__________________________________________________________________"
       puts " "
       puts "---------------- Welcome to the Cat Catalogue -------------------"
@@ -12,34 +18,69 @@ module CatBreeds
       puts " __ Please type 'show list' for the full list of cat breeds"
       
       puts " __ Type 'close' to exit the application"
+    end 
 
-      input = gets.chomp 
+def user_input
+   puts "Please enter your input below this line"
+   puts "***************************************"
+   puts "         "
+      input = gets.strip.downcase 
 
-      if input == 'show list'
-        puts "All breeds"
-
-        CatBreeds::Scraper.scrape_breeds
-        CatBreeds::Breeds.all.each.with_index do |breed, index|
-          puts "#{index} - #{breed.name}"
-        end
-
-        print 'Please select a breed: '
-        breed = gets.chomp.to_i 
-        selected_breed = CatBreeds::Breeds.all[breed]
-
-
-        puts "You selected #{selected_breed.name}"
-        puts "Getting breed info"
-
-        breed_info = CatBreeds::Scraper.scrape_breed_info(selected_breed.url)
-        puts breed_info
-
-      elsif input = "close"
-
-        exit
-      end
-    
-    
+      
+      if input == "show list"
+        puts "All of the amazing cat breeds:"
+          list_breeds
+          select_breed
+          another_breed
+     
+      elsif input == "close"
+      puts "Thanks for stopping by. Goodbye now!"
+      exit
     end
   end 
+
+
+      
+     def another_breed
+        puts "**************************"
+        puts "Would you like to learn about another cat breed? Enter Y/N"
+
+        input = gets.strip.downcase
+
+          if input == "y"
+            puts "     "
+             list_breeds
+             select_breed
+             another_breed 
+
+          elsif input == "n"
+          puts "Thanks for stopping by. Goodbye now!"
+          sleep (1) 
+          exit
+      end 
+   end 
+
+ 
+    
+   def list_breeds
+        CatBreeds::Scraper.scrape_breeds
+        CatBreeds::Breeds.all.each.with_index do |breed, index|
+          puts "#{index + 1} - #{breed.name}"
+    end 
+
+  def select_breed
+
+    print 'Please select a breed: '
+     breed = gets.chomp.to_i 
+     selected_breed = CatBreeds::Breeds.all[breed]
+
+
+     puts "You selected #{selected_breed.name}"
+     puts "Getting breed info"
+
+      breed_info = CatBreeds::Scraper.scrape_breed_info(selected_breed.url)
+    puts breed_info
+ end
+end 
 end
+end 
